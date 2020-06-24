5b27b38e562d83f9633a06d341b9d8b5f643d485
Huang <Xiaotian.Huang1@elekta.com>
2020-06-24 17:34:59 +0800

    MAIN.mlx

--- MI_leafspeed_TEST.m
+++ MI_leafspeed_TEST.m
@@ -19,7 +19,7 @@ for j=1:VMAT_PLN_INFO.Total_CPs-1
            delta_gantry = 360 + VMAT_PLN_INFO.CP_info{j+1,1} - VMAT_PLN_INFO.CP_info{j,1}; % gantry interval
        end
        delta_MU = VMAT_PLN_INFO.CP_info{j+1,2} - VMAT_PLN_INFO.CP_info{j,2};     % MU interval
-       max_MU_interval = delta_gantry/Max_gantry_speed*Max_dose_rate/60;
+       max_MU_interval = 2.0341/Max_gantry_speed*Max_dose_rate/60;
        
        if delta_MU <= max_MU_interval
            
@@ -55,7 +55,8 @@ for jj=j+1:VMAT_PLN_INFO.Total_CPs-1
        
        if delta_MU <= max_MU_interval
            
-           time_interval(jj) = delta_gantry/Max_gantry_speed; % time interval
+           % time_interval(jj) = delta_gantry/Max_gantry_speed; % time interval
+           time_interval(jj) = 2.0341/Max_gantry_speed;
            
        else
            
@@ -78,7 +79,7 @@ end
 
 %% calculate modulation index for leaf speed
 
-MLC_speed_mat = zeros(size(VMAT_PLN_INFO.CP_info{1, 3},1),VMAT_PLN_INFO.Total_CPs-1); % construct the MLC speed matrix (num of MLC leaves x num of control points)
+MLC_speed_mat_1 = zeros(size(VMAT_PLN_INFO.CP_info{1, 3},1),VMAT_PLN_INFO.Total_CPs-1); % construct the MLC speed matrix (num of MLC leaves x num of control points)
 
 for jj=1:VMAT_PLN_INFO.Total_CPs-1
     MLC_speed_mat_1(:,jj) = abs((VMAT_PLN_INFO.CP_info{jj, 3}-...
@@ -90,14 +91,14 @@ end
 %         VMAT_PLN_INFO.CP_info{jj+1, 3}))./time_interval(jj);  %mm/s
 % end
 
-MLC_speed_mat(:,flag) = 0;
-VMAT_PLN_INFO.MLC_speed_std = std(MLC_speed_mat,0,2);
+MLC_speed_mat_1(:,flag) = 0;
+VMAT_PLN_INFO.MLC_speed_std = std(MLC_speed_mat_1,0,2);
 
 
 % initialize the Z distribution
 f = 0:0.01:2;
 Z = zeros(size(VMAT_PLN_INFO.MLC_speed_std,1),length(f));
-X = abs(MLC_speed_mat); % absolute value of leaf speed
+X = abs(MLC_speed_mat_1); % absolute value of leaf speed
 
 for ii = 1:size(Z,1)
     f_threshold = f*VMAT_PLN_INFO.MLC_speed_std(ii); % the serial number of leaf position (e.g 2nd leaf of 160 leaves)
@@ -112,7 +113,7 @@ VMAT_PLN_INFO.MI_S_05 = sum(0.01*sum(Z(:,1:idx2),2));
 VMAT_PLN_INFO.MI_S_1 = sum(0.01*sum(Z(:,1:idx3),2));
 VMAT_PLN_INFO.MI_S_2 = sum(0.01*sum(Z(:,1:idx4),2));
 
-VMAT_PLN_INFO.MLC_speed_mat = MLC_speed_mat; % a matrix (160 leaf x 219 control points)
+VMAT_PLN_INFO.MLC_speed_mat_1 = MLC_speed_mat_1; % a matrix (160 leaf x 219 control points)
 VMAT_PLN_INFO.CP_time_interval = time_interval; % time interval between different control points
 
 % calculate average leaf speed and standard deviation of leaf speed
