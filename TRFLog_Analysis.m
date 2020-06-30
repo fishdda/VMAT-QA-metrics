3e1e24c9b08fd41620a95b050e9cc2f51dcc51d5
Huang <Xiaotian.Huang1@elekta.com>
2020-06-30 12:18:24 +0800

    Add a trf file

---
+++ TRFLog_Analysis.m
@@ -0,0 +1,3 @@
+% This TRF log analysis tool was used to analysis the csv file transfered
+% from trf generated via Elekta Linac Integrity. 
+load C:\GitFolder\VMAT-QA-metrics\example\test_case\VMAT1Arc\VMAT202003181Arc.mat;
